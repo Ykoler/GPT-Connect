c34743afefd485a781b72be580f6620d82e32e3d
Ykoler <kolae@yatmal.co.il>
2023-08-16 22:21:56 +0300

    added requierments and changed imports

--- imports.py
+++ imports.py
@@ -1,18 +1,11 @@
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium import webdriver
 import time
 from random import randint
 from bs4 import BeautifulSoup
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
 import time
 from random import *
 import numpy as np
 import scipy.interpolate as si
-import os
-import threading
 import undetected_chromedriver as uc
-from fake_useragent import UserAgent
-import sys
